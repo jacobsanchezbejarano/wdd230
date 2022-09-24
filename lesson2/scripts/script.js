46a487775d02f2ffd543d894a7fc61c659fbb3ab
jacobsanchezbejarano <jake_76847@hotmail.com>
2022-09-23 22:04:15 -0400

    updating content

--- lesson2/scripts/script.js
+++ lesson2/scripts/script.js
@@ -34,7 +34,10 @@ layout1.principle = "Visual Hierarchy";
 layout1.organization = "Apple";
 layout1.link = "https://www.apple.com/la/";
 layout1.image = "screenshot_apple_mobile.png";
-layout1.description = "Visual hierarchy is the principle of arranging elements to show their order of importance. Designers structure visual characteristics—e.g., menu icons—so users can understand information easily. By laying out elements logically and strategically, designers influence users’ perceptions and guide them to desired actions.";
+layout1.description = 
+"Visual hierarchy is the principle of arranging elements to show their order of importance. This "
++"website is a good example of that concept, showing in the first place "
++"its most important product, covering all the screen, and then the following items.";
 
 
 let layout2 = new Layout();
@@ -43,7 +46,7 @@ layout2.principle = "Alignment";
 layout2.organization = "DHL";
 layout2.link = "https://www.dhl.com/bo-es/home.html";
 layout2.image = "screenshot_dhl_mobile.png";
-layout2.description = "Alignment allows an observer to read the image more easily and clearly. This is achieved by aligning objects to establish order and avoid haphazardly positioning items in the image.";
+layout2.description = "Alignment allows an observer to read the image more easily and clearly. This is achieved by aligning objects to establish order and avoid haphazardly positioning items in the image. I liked the alignment of boxes in this website, because it's very clear how to navigate through the site and it is easy to read the content in the boxes.";
 
 
 let layout3 = new Layout();
@@ -52,7 +55,7 @@ layout3.principle = "Hick's Law";
 layout3.organization = "Adidas";
 layout3.link = "https://www.adidas.com/us";
 layout3.image = "screenshot_adidas_mobile.png";
-layout3.description = 'The more options an user have to do, the less time he will stay in the website. Good designers try to respect the user\'s time and in this way improve the user\'s experience. "Users bombarded with choices have to take time to interpret and decide, giving them work they don’t want."';
+layout3.description = 'The more options an user have to do, the less time he will stay in the website. For that reason, I liked this website, there are just three main calls to action, with very simple titles. A good design should have the least options in the screen to increase the user\'s navigation.';
 
 
 
