49818570eb89bb012801671f3cbc18f2487db1c1
jacobsanchezbejarano <jake_76847@hotmail.com>
2022-10-21 02:54:02 -0400

    updated

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,26 +1,30 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c15db17fc3msh24a23cb9ae56214p151591jsn7bbb26185115',
-		'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
-	}
-};
+function set_weather_data(){
+    const T = parseFloat(document.querySelector('#el-temperature').textContent);
+    const V = parseFloat(document.querySelector('#el-windspeed').textContent);
+    
+    console.log(T);
+    console.log(V);
+    
+    const el_wind_chill = document.querySelector('#el-windchill');
 
-fetch('https://yahoo-weather5.p.rapidapi.com/weather?lat=-17.770253&long=-63.182201&format=json&u=c', options)
-	.then(response => response.json())
-	.then(response => set_weather_data(response))
-	.catch(err => console.error(err));
+    let windchill = 0; 
 
-function set_weather_data(response){
-    const el_temperature = document.querySelector('#temperature');
-    const el_description_temperature = document.querySelector('#description-temperature');
-    const el_wind_speed = document.querySelector('#wind-speed');
+    if (T <= 10 && V > 4.8){
 
-    el_temperature.textContent = response.current_observation.condition.temperature + "°C";
-    el_description_temperature.textContent = response.current_observation.condition.text;
-    el_wind_speed.textContent = "Wind speed: " + response.current_observation.wind.speed + " km/h";
+        windchill = 13.12 + 0.6215 * T - 11.37 * V ** 0.16 + 0.3965 * T * V ** 0.16;
+        windchill = windchill.toFixed(1);
+
+    }else{
+
+        windchill = "N/A";
+    
+    }
+    
+    el_wind_chill.textContent = windchill;
 
 }
+
+set_weather_data();
 
 
 date = new Date(Date.now());
@@ -65,3 +69,47 @@ if (weekday === 1 || weekday === 2){
     article.setAttribute('class', 'banner-message');
 }
 
+
+/*const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '',
+		'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
+	}
+};
+
+fetch('https://yahoo-weather5.p.rapidapi.com/weather?lat=-17.770253&long=-63.182201&format=json&u=c', options)
+	.then(response => response.json())
+	.then(response => set_weather_data(response))
+	.catch(err => console.error(err));
+
+function set_weather_data(response){
+    const el_temperature = document.querySelector('#temperature');
+    const el_description_temperature = document.querySelector('#description-temperature');
+    const el_wind_speed = document.querySelector('#wind-speed');
+    const el_wind_chill = document.querySelector('#wind-chill');
+
+    const T = response.current_observation.condition.temperature;
+    const V = response.current_observation.wind.speed;
+
+    let windchill = 0; 
+
+    if (T <= 10 && V > 4.8){
+
+        windchill = 13.12 + 0.6215 * T - 11.37 * V ** 0.16 + 0.3965 * T * V ** 0.16;
+        windchill = windchill.toFixed(1);
+
+    }else{
+
+        windchill = "N/A";
+    
+    }
+    
+    el_temperature.textContent = T + "°C";
+    el_description_temperature.textContent = response.current_observation.condition.text;
+    el_wind_speed.textContent = "Wind speed: " + V + " km/h";
+    el_wind_chill.textContent = "Wind chill: " + windchill;
+
+}
+
+*/
