dd14709d14287d8d3ec89dc4d25948c337f9f4bd
jacobsanchezbejarano <jake_76847@hotmail.com>
2022-11-12 13:19:45 -0400

    json

--- chamber/scripts/json.js
+++ chamber/scripts/json.js
@@ -4,7 +4,7 @@ const cards = document.querySelector('.grid');
 
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
+const display = document.querySelector(".grid");
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
