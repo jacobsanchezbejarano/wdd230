a48168f0c890169c7ae3396097e7af74959fbbc6
jacobsanchezbejarano <jake_76847@hotmail.com>
2022-11-11 21:02:43 -0400

    fixing

---
+++ chamber/scripts/join.js
@@ -0,0 +1,10 @@
+const btitle = document.getElementById("bpositiontitle");
+
+btitle.addEventListener("input", (event) => {
+  if (btitle.validity.patternMismatch) {
+    btitle.setCustomValidity("Field expects 7 or more characters");
+    btitle.reportValidity();
+  } else {
+    btitle.setCustomValidity("");
+  }
+});
