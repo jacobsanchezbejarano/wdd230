218b1e25d224103f1e2f8d023b78c4ef7bcdef10
jacobsanchezbejarano <jake_76847@hotmail.com>
2022-11-19 13:37:06 -0400

    fix

--- chamber/scripts/lazyloading.js
+++ chamber/scripts/lazyloading.js
@@ -1,10 +1,4 @@
-const imagesToLoad = document.querySelectorAll('source[data-src]');
-const loadImages = (image) => {
-  image.setAttribute('srcset', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
-};
+
 
 const imagesToLoad2 = document.querySelectorAll('img[data-src]');
 const loadImages2 = (image) => {
@@ -13,26 +7,6 @@ const loadImages2 = (image) => {
     image.removeAttribute('data-src');
   };
 };
-
-
-if ('IntersectionObserver' in window) {
-  const observer = new IntersectionObserver((items) => {
-    items.forEach((item) => {
-      if (item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-      }
-    });
-  });
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
-  });
-} else {
-  imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
-}
-
 
 if ('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items) => {
