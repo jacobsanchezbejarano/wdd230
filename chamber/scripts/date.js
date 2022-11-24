a0d312b63793d952a0372bf6a092090310b11a46
jacobsanchezbejarano <jake_76847@hotmail.com>
2022-11-23 21:51:56 -0400

    contact

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -20,3 +20,6 @@ day: 'numeric' };
 document.querySelector('#datetime').textContent = 
 new Date(Date.now()).toLocaleString('en-UK',opciones2);
 
+document.querySelector('.logo-footer').addEventListener('click',() => {location.assign('https://jacobsanchezbejarano.github.io/wdd230/chamber/');});
+document.querySelector('.logo').addEventListener('click',() => {location.assign('https://jacobsanchezbejarano.github.io/wdd230/chamber/');});
+document.querySelector('.header-title').addEventListener('click',() => {location.assign('https://jacobsanchezbejarano.github.io/wdd230/chamber/');});
